0ea2f9c3e6cb9cf6d50884d06c864b75c3135032
ezgiozturan <ezgiozturan05@gmail.com>
2024-11-25 17:53:11 +0300

    redux store kuruldu

---
+++ budget-tracker/src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import budgetReducer from "./features/budgetSlice";
+const store = configureStore({
+  reducer: {
+    budget: budgetReducer,
+  },
+});
+export default store;
